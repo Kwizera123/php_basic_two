854adc27ea17e46c087fadc86c1ddaca9e7961b8
Kwizera Theo <kwizerasinapy49@gmail.com>
2025-03-28 21:24:04 +0200

    Database setup added

--- mvc-app/config/config.php
+++ mvc-app/config/config.php
@@ -1,5 +1,20 @@
 <?php 
 
+return [
+  'database' => [
+      'host' => 'localhost',
+      'database' => 'mvc_app_db',
+      'username' => 'root',
+      'password' => '',
+      'port' => 3306,
+      'chartset' => 'utf8mb4'
+  ],
+  'app' => [
+      'base_url' => 'http://mvc-app.local/',
+      'debug' => true,
 
-const BASE_URL = "http://mvc-app.local/";
+    ]
+  ];
+
+
 
